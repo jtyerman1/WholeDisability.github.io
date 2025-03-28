51f85a18f04ad130bf393598074c2631d478f9cf
jtyerman1 <jack.tyerman96@gmail.com>
2025-03-28 16:23:29 +1100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = htmlspecialchars(trim($_POST["name"]));
+    $email = htmlspecialchars(trim($_POST["email"]));
+    $message = htmlspecialchars(trim($_POST["message"]));
+
+    $to = "info@wholedisability.com.au";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Your message has been sent successfully.";
+    } else {
+        echo "There was a problem sending your message. Please try again.";
+    }
+}
+?>
